1c4415cbb6041308fc72944face50fc2d1515aba
Harsh Shah <81619640+Harshs1911@users.noreply.github.com>
2024-02-12 19:18:20 +0000

    learn variable

---
+++ .devcontainer/01_Basics/01_Variables.js
@@ -0,0 +1,14 @@
+var name = "harsh"
+name = "vansh";
+
+let nameV = " vishal"
+name = "vishal"
+
+const nameg = "Gaurav"
+name = "Prithvi"
+
+console.log([name]);
+
+/* dont use var
+
+because problem's create in block scope and function scope ( " { } " ) */
