7dae9bad237c32525e348804af143167997631c3
Harsh Shah <81619640+Harshs1911@users.noreply.github.com>
2024-02-13 18:26:27 +0000

    learn data type

---
+++ .devcontainer/01_Basics/02_Datatypes.js
@@ -0,0 +1,7 @@
+//There are five types of data types
+// 1. String  ( "abcdefghijklmnoprstuwxyz")
+// 2. Number ( 123456789)
+// 3. boolean ( if the situation is true or false)
+// 4. undefined ( when the variables not assign a value that time )  (Any variable that has not been assigned a value has the value undefined.)
+// 5. null ( Null is a special data type which can have only one value: NULL. A variable of data type NULL is a variable that has no value assigned to it.)
+console.log( Datatype)
