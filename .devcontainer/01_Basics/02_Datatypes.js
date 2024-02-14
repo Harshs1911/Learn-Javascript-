4b05a7beab8f52d3bdaacf6d4226dee566b03965
Harsh Shah <81619640+Harshs1911@users.noreply.github.com>
2024-02-14 18:32:49 +0000

    converation number to string

--- .devcontainer/01_Basics/02_Datatypes.js
+++ .devcontainer/01_Basics/02_Datatypes.js
@@ -4,4 +4,9 @@
 // 3. boolean ( if the situation is true or false)
 // 4. undefined ( when the variables not assign a value that time )  (Any variable that has not been assigned a value has the value undefined.)
 // 5. null ( Null is a special data type which can have only one value: NULL. A variable of data type NULL is a variable that has no value assigned to it.)
-console.log( Datatype)
+//console.log( Datatype)
+
+let somenumber = "333abc"
+
+let valueInNumber = Number(somenumber)
+console.log(typeof valueInNumber)
